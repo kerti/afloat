2c7d2065086344e02851517b34e5384938278bf9
Raditya Kertiyasa <adit.kerti@gmail.com>
2026-09-17 08:20:21 +0700

    Initializr placed into backend-kotlin folder.

---
+++ backend-kotlin/src/test/kotlin/dev/kerti/afloat/AfloatApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.kerti.afloat
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AfloatApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
